95fcbc7181e20b95c633afcf4fa834ccf6e7e11f
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-10 16:50:53 -0600

    update server CORS policy

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ app.use(
         origin: "http://127.0.0.1:5173",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 )
 
