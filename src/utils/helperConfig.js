895f67c1c512cddfa04ba6985a72bc53a962707f
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-25 19:05:10 -0600

    update dev URI

--- src/utils/helperConfig.js
+++ src/utils/helperConfig.js
@@ -1,6 +1,6 @@
 // Set default protected mongo uri for development branch
 export const MONGO_DEV_URI =
-    "mongodb+srv://cloudwalker0013:A3GeYLhO5pOjF6nc@development-data.e0khqcy.mongodb.net/?retryWrites=true&w=majority&appName=Development-Data"
+    "mongodb+srv://devUser:5JEq0Pb2YYQHJ93i@development-data.e0khqcy.mongodb.net/?retryWrites=true&w=majority&appName=Development-Data"
 
 // Set default local host port
 export const PORT = 5000
