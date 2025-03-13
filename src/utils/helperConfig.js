a396de3d34fcf6e7201f667cb5682c0c0f46d293
kotesh-arya <koteswarraomudila@gmai.com>
2025-03-13 23:37:44 +0530

    REFACTOR: Email configuration via env

--- src/utils/helperConfig.js
+++ src/utils/helperConfig.js
@@ -7,3 +7,14 @@ export const PORT = 5000
 
 // Set default local client port
 export const CLIENT_PORT = 3000
+
+
+export const mailConfig = {
+    mailService: process.env.MAIL_SERVICE,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    resetLink: process.env.RESET_LINK,
+    from: process.env.MAIL_FROM,
+};
