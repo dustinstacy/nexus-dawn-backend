8d5059b39cd8815e01dbb93c6473d440b65cf330
kotesh-arya <koteswarraomudila@gmai.com>
2025-03-14 13:19:28 +0530

    FIX: Environment variables accessing

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -5,25 +5,24 @@ import dotenv from "dotenv";
 dotenv.config(); // Load environment variables
 
 
-const { mailService, auth, resetLink, from } = mailConfig;
-
+const { mailService, auth, from } = mailConfig;
 const transporter = nodemailer.createTransport({
-    service: mailService,
-    auth: {
-      user: auth.user,
-      pass: auth.pass,
-    },
-  });
+  service: mailService,
+  auth: {
+    user: auth.user,
+    pass: auth.pass,
+  },
+});
 
 export const sendResetEmail = async (email, token) => {
-    const resetLink = `http://localhost:3000/password-reset?token=${token}`;
+  const resetLink = `http://localhost:3000/password-reset?token=${token}`;
 
-    const mailOptions = {
-        from: "kotesharya1@gmail.com",
-        to: email,
-        subject: "Password Reset Request",
-        text: `Click the link to reset your password: ${resetLink}`,
-    };
+  const mailOptions = {
+    from: "kotesharya1@gmail.com",
+    to: email,
+    subject: "Password Reset Request",
+    text: `Click the link to reset your password: ${resetLink}`,
+  };
 
-    return transporter.sendMail(mailOptions);
+  return transporter.sendMail(mailOptions);
 };
